f84bf159af1785f2da91f3d2b2f0531813760215
cleiton <cleiton.oliveira@polisys.com.br>
2018-08-24 17:13:08 -0300

    data-binding atualizado com a aula 11 de Event binding

--- data-binding/src/app/data-binding/data-binding.component.ts
+++ data-binding/src/app/data-binding/data-binding.component.ts
@@ -3,22 +3,59 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-data-binding',
   templateUrl: './data-binding.component.html',
-  styleUrls: ['./data-binding.component.css']
+  styleUrls: ['./data-binding.component.css'],
+  /*styles: [
+    `
+      .highlight{
+        background-color: yellow;
+        font-weight: bold
+      }
+    `
+  ]*/
 })
+
+
 export class DataBindingComponent implements OnInit {
 
-  url: string= "http://loiane.com"
+  url: string = "http://loiane.com"
 
   cursoAngular: boolean = true;
 
-  urlImagem:string = 'https://s2.glbimg.com/tLXQrHyP20CDJBCPBChJJcmcC88=/e.glbimg.com/og/ed/f/original/2018/04/06/eso1810a.jpg'
+  urlImagem: string = 'https://s2.glbimg.com/tLXQrHyP20CDJBCPBChJJcmcC88=/e.glbimg.com/og/ed/f/original/2018/04/06/eso1810a.jpg'
 
-  getValor(){
+  valorAtual: String = "";
+
+  ValorSalvo = '';
+
+  isMouseOver: boolean = false;
+
+  onMouseOverOut() {
+
+    this.isMouseOver = !this.isMouseOver;
+
+  }
+  salvarValor(valor) {
+
+    this.ValorSalvo = valor;
+
+  }
+
+  getValor() {
     return 1;
   }
 
-  getCurso(){
-   return true;
+  getCurso() {
+    return true;
+  }
+
+  botaoClicado() {
+    alert("botão clicado")
+  }
+
+  onKeyUp(evento: KeyboardEvent) {
+
+    this.valorAtual = (<HTMLInputElement>evento.target).value
+
   }
 
   constructor() { }
